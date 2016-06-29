2f1f06ab3247b72c35e0108b3b09bf4ae40dde5d
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-06-29 13:54:45 +0200

    Added regular expressions support (Substring function) for PostgreSQL in Doctrine

---
+++ DoctrineExtensions/Functions/Substring.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Librinfo\DoctrinePgsqlBundle\DoctrineExtensions\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode,
+    Doctrine\ORM\Query\Lexer;
+
+/**
+ * Pattern matching function
+ * Usage: SUBSTRING(field, regexp)
+ * Outputs: SUBSTRING(field FROM regexp)
+ */
+class Substring extends FunctionNode
+{
+    protected $field;
+    protected $regexpExpression;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->regexpExpression = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->valueExpression = $parser->StringExpression();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'SUBSTRING(' . $this->field->dispatch($sqlWalker) . ' FROM ' .
+            $sqlWalker->walkStringPrimary($this->regexpExpression) . ')';
+    }
+
+}
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Features
 For the moment, the only feature of this bundle is:
 
 - replacing LIKE keyword by ILIKE (Postgresql specific) in sql queries in order to have case insensitive comparisons
+- Substring function (regular expressions): "substring(field, regexp)" outputs "substring(field from regexp)". Postgresql specific
